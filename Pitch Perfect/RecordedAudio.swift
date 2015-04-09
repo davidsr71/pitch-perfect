b7adbc8b3f12c08edc861ae7abaf8b0c42b416fd
davidsr71 <dmhughes@optusnet.com.au>
2015-04-10 07:37:03 +1000

    Committed by Udacity

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,18 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by David Hughes on 6/04/2015.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
+
+
